69a4a29866a5548e126fdc24e96b9064fd95150c
gustavo_s_braga <gustavo_s_braga@estudante.sesisenai.org.br>
2023-06-14 08:52:15 -0300

    arquivo1 finalizado

---
+++ Arquivo1.js
@@ -0,0 +1,7 @@
+function numberGreater(n1, n2) {
+  return n1 > n2;
+}
+
+numberGreater(3, 2)
+  ? console.log("O primeiro número é maior que o segundo")
+  : console.log("O primeiro número não é maior que o segudno");
