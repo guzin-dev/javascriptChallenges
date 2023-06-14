4c0ac2f31aeee2f39af3494e26b582da93d1a452
gustavo_s_braga <gustavo_s_braga@estudante.sesisenai.org.br>
2023-06-14 09:32:01 -0300

    arquivo4 finalizado

---
+++ Arquivo4.js
@@ -0,0 +1,7 @@
+function inverseBooleanAndNumber(handleVar) {
+  if(typeof(handleVar) == "boolean") return !handleVar
+  if(typeof(handleVar) == "number") return handleVar*-1
+  return "Tipo de variável não permitida"
+}
+
+console.log(inverseBooleanAndNumber(false))
